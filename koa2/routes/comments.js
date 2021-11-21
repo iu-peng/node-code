a6da0f8a1b72ce5111cc6f781c4930a073386d14
roc <rocmail>
2021-11-21 16:23:07 +0800

    koa 添加列表get post

---
+++ koa2/routes/comments.js
@@ -0,0 +1,29 @@
+const router = require("koa-router")();
+
+router.prefix("/api");
+
+router.get("/list", async (ctx) => {
+  const query = ctx.query; // get 的？后的参数
+  console.log("参数", query);
+  // ctx.body = "api list"; // 返回的是文本格式
+  // 返回的是json格式
+  ctx.body = {
+    errno: 0,
+    data: [
+      { content: "内容1234r", user: "roc" },
+      { content: "内容1234r", user: "roc" },
+      { content: "内容1234r", user: "roc" },
+    ],
+  };
+});
+router.post("/create", async (ctx) => {
+  const body = ctx.request.body; // post 的内容部分
+  console.log("post body", body);
+  // ctx.body = "api list"; // 返回的是文本格式
+  ctx.body = {
+    errno: 0,
+    message: "成功了",
+  };
+});
+
+module.exports = router;
