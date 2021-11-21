9feb6cd11f19dc04248cba8df848939a5178c96d
roc <rocmail>
2021-11-21 15:31:32 +0800

    feat: koa初始化

--- index.js
+++ index.js
@@ -1,41 +1,10 @@
-const http = require("http");
-const queryString = require("querystring");
-const port = 3000;
+const Koa = require("koa");
 
-const server = http.createServer((req, res) => {
-  const params = queryString.parse(req.url.split("?")[1]);
-  console.log("收到请求", params);
+const app = new Koa();
 
-  const path = req.url.split("?")[0];
-  const method = req.method;
-  if (path === "/api/list" && method === "GET") {
-    const result = [
-      {
-        user: "张三",
-        content: "内容1",
-      },
-    ];
-    res.writeHead(200, { "Content-type": "application/json" });
-    res.end(JSON.stringify(result));
-  }
-  if (path === "/api/create" && method === "GET") {
-    const result = {
-      date: "success",
-    };
-    const bodyStr = "";
-    res.on("data", (chunk) => {
-      bodyStr += chunk;
-    });
-    res.on("close", () => {
-      console.log("结束了");
-      res.writeHead(200, { "Content-type": "application/json" });
-      res.end(JSON.stringify(result));
-    });
-    return;
-  }
-
-  res.writeHead(404, { "Content-type": "text/plain" });
-  res.end("404 not found");
+app.use(async (ctx) => {
+  ctx.body = "hello koa";
 });
-server.listen(port);
-console.log(`服务启动了 请访问 http://localhost:${port}`);
+
+app.listen(3000);
+console.log("服务启动了");
